40052a0e18b8314b3d81183603fb60a150b87871
nagaraju <naga.gollapelli@gmail.com>
2017-04-21 00:57:47 +0530

    Enum updated with data

--- nagarajumaven/src/main/java/com/naga/Menu.java
+++ nagarajumaven/src/main/java/com/naga/Menu.java
@@ -1,5 +1,31 @@
 package com.naga;
 
+import java.math.BigDecimal;
+
 public enum Menu {
 
+	COLA("Cola", "Cold", BigDecimal.valueOf(0.5)),
+	COFFEE("Coffee","Hot", BigDecimal.ONE),
+	CHINESE_SANDWICH("Chinese Sandwich","Cold", BigDecimal.valueOf(2)),
+	STEAK_SANDWICH("Steak Sandwich", "Hot",BigDecimal.valueOf(4.5));
+	
+	private String name;
+	private String type;
+	private BigDecimal price;
+	private Menu(String name, String type, BigDecimal price) {
+		this.name = name;
+		this.type = type;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	
+	
 }
